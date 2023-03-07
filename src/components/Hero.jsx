f40aa2bf29da3c729b05586ffffac3eaf7883b1d
ifemi27 <opatolaifeoluwav@gmail.com>
2023-03-07 00:10:34 -0800

    hero copy edited

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,25 +7,7 @@ const Hero = () => {
     <div className=' py-2 flex container max-w-[1280px] mx-auto'>
         <div  className='bg-[#FCF1EA] py-[10%] w-[100%] lg:w-[60%] px-[9%]'>
             <h1 className='font-bold text-5xl mb-6'>Hello and welcome to Yetti Speaks </h1>
-            <p>I'm so glad you found your way here. This is a place where I share my thoughts, musings, and experiences on a variety of topics, from mental health to personal growth to travel and everything in between.
-
-As someone who's passionate about self-discovery and living life to the fullest, I created Yetti Speaks as a platform to share my journey and connect with others who are on a similar path.
-
-Here, you'll find stories, insights, and tips on how to navigate the ups and downs of life, and how to find joy, meaning, and purpose along the way.
-
-So, whether you're here for inspiration, advice, or just a good read, I hope you'll find something that resonates with you.
-
-Thank you for joining me on this journey, and I can't wait to see where it takes us.
-
-Yours in adventure,
-
-Yetti.
-
-
-
-
-
-</p>
+            <p>I'm so glad you found your way here. This is a place where I share my thoughts, musings, and experiences on a variety of topics, from mental health to personal growth to travel and everything in between.</p>
             <div className='flex gap-6 mt-6 items-center'>
                 <button className='py-2 px-4 bg-[#af6c44a1] rounded-full hover:bg-[#9b6f56] text-white'>Read My Blogs</button>
                 <a className='underline decoration-[#9b6f56bd] decoration-2 underline-offset-8' href="">Visit My Shop</a>
